30c8b776a405ed056d68d52a54ad80b2ef253f6a
aminemoh <alm.amine@gmail.com>
2018-11-06 17:07:49 +0100

    originefngapp

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,55 @@
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { FetchEmployeeComponent } from './components/fetch-employee/fetch-employee.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { EmpserviceService } from './services/empservice.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    AddEmployeeComponent,
+    FetchEmployeeComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({
+      appId: 'ng-cli-universal'
+    }),
+    CommonModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: HomeComponent,
+      pathMatch: 'full'
+    }, {
+      path: 'home',
+      component: HomeComponent
+    }, {
+      path: 'fetch-employee',
+      component: FetchEmployeeComponent
+    }, {
+      path: 'add-employee',
+      component: AddEmployeeComponent
+    }, {
+      path: 'employee/edit/:id',
+      component: AddEmployeeComponent
+    }, {
+      path: '**',
+      redirectTo: 'home'
+    }])
+  ],
+  providers: [EmpserviceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }  
---
+++ Models/Angular5_core2Context.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using EfNgApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EfNgApp.Models
+{
+    public partial class Angular5_core2Context : DbContext
+    {
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+		public static string GetConnectionString()
+		{
+			return Startup.ConnectionString;
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId);
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId);
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
